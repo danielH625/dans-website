f76b96d3e30f8ee8dbc99237ddb98f67a5d9a399
danielH625 <danh_tools@pm.me>
2026-05-07 10:40:02 -0400

    Added PyChef
    Added the button and description for PyChef under my projects tab.

--- pages/projects.py
+++ pages/projects.py
@@ -6,6 +6,10 @@ st.divider()
 col1, col2 = st.columns([0.2, 0.8])
 with col1:
     st.link_button("Hash-Identifier", "https://github.com/danielH625/Hash-Identifier")
+    st.link_button("PyChef", "https://github.com/danielH625/PyChef/tree/main")
 
 with col2:
     st.write("CLI tool that can identify hashes individually or from a file.")
+    st.write(
+        "CLI tool that encodes and decodes various ciphers. (inspired by CyberChef)"
+    )
